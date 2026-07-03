12ce87ecd71239edae2820e0f41e8656c8e916dd
Marcel Behrmann <info@eldoran.dev>
2026-07-03 20:53:36 +0200

    simplify blog styling to use tailwind typography

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -11,50 +11,22 @@ const { data: doc } = await useAsyncData(route.path, () => {
       <profile />
     </aside>
     <main class="right-column">
-      <article v-if="doc">
-        <h1>{{ doc.title }}</h1>
-        <BlogSummary :content="doc" />
-        <div
-          class="relative not-prose rounded-2xl overflow-hidden"
-          v-if="doc.cover"
-        >
-          <img :src="doc.cover" decoding="async" />
-        </div>
-        <ContentRenderer :value="doc" class="content" />
+      <article v-if="doc" class="prose dark:prose-invert">
+        <header>
+          <h1>{{ doc.title }}</h1>
+          <BlogSummary :content="doc" />
+
+          <div
+            class="relative not-prose rounded-2xl overflow-hidden"
+            v-if="doc.cover"
+          >
+            <img :src="doc.cover" decoding="async" />
+          </div>
+        </header>
+        <ContentRenderer :value="doc" />
       </article>
     </main>
   </div>
 </template>
 
-<style lang="postcss" scoped>
-:deep() h1 {
-  @apply text-3xl font-extrabold leading-tight text-gray-900 lg:mb-1 lg:text-4xl dark:text-white;
-}
-
-:deep() h2 {
-  @apply mb-3 text-2xl lg:text-3xl font-semibold;
-}
-
-:deep() h2 > a,
-h3 > a {
-  @apply text-black dark:text-white;
-}
-
-:deep() h3 {
-  @apply mb-2 text-xl lg:text-2xl;
-}
-
-:deep() p {
-  @apply my-4;
-}
-
-:deep() ol {
-  @apply list-decimal my-4;
-}
-
-div.content {
-  @apply p-0 lg:text-lg leading-4;
-
-  text-align: justify;
-}
-</style>
+<style lang="postcss" scoped></style>
