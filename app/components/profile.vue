93d251dc35ca18534a10ca3fe9477bdbe0336a56
Marcel Behrmann <info@eldoran.dev>
2026-07-06 20:11:46 +0200

    simplify profile

--- app/components/profile.vue
+++ app/components/profile.vue
@@ -4,20 +4,15 @@
     <p class="profile-name">Marcel Behrmann</p>
     <p class="profile-sub">Games & Apps</p>
     <div class="social-profiles">
-      <a href="https://github.com/EldoranDev" target="_blank" rel="noopener">
-        <Icon name="fa6-brands:github-square" class="profile-icon" />
-      </a>
-      <a
+      <SocialLink
+        icon="fa6-brands:github-square"
+        href="https://github.com/EldoranDev"
+      />
+      <SocialLink
+        icon="fa6-brands:linkedin"
         href="https://www.linkedin.com/in/marcel-behrmann/"
-        target="_blank"
-        rel="noopener"
-      >
-        <Icon name="fa6-brands:linkedin" class="profile-icon" />
-      </a>
+      />
     </div>
-
-    <!-- <nuxt-link to="/blog">Blog</nuxt-link> -->
-
     <theme-switcher />
   </div>
 </template>
@@ -28,7 +23,7 @@
 }
 
 .avatar {
-  @apply rounded-full shadow-md shadow-lg mx-auto;
+  @apply rounded-full shadow-lg mx-auto;
   width: 125px;
   height: 125px;
 
@@ -50,10 +45,5 @@
     max-width: 100%;
     min-width: default;
   }
-}
-
-.profile-icon {
-  @apply w-9 h-9 relative inline-block text-black dark:text-white hover:text-blue-600;
-  @apply hover:text-blue-600 !important;
 }
 </style>
---
+++ app/components/social-link.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { href, icon } = defineProps<{
+  href: string;
+  icon: string;
+}>();
+</script>
+
+<template>
+  <a :href="href" target="_blank" rel="noopener">
+    <Icon :name="icon" class="profile-icon" />
+  </a>
+</template>
+
+<style lang="postcss" scoped>
+.profile-icon {
+  @apply w-9 h-9 relative inline-block text-black dark:text-white hover:text-blue-600;
+  @apply hover:text-blue-600 !important;
+}
+</style>
